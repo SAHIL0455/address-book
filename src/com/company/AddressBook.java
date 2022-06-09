b5f1d80bc3330abf23a4d02ad9c529081c8a2f79
SAHIL0455 <sahilpawar1004@gmail.com>
2022-06-09 14:07:20 +0530

    ADDED: Contacts information in Address Book

--- src/com/company/AddressBook.java
+++ src/com/company/AddressBook.java
@@ -1,6 +1,10 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class AddressBook {
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         System.out.println("Welcome the Address Book");
     }
