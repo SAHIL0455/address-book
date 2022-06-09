cae6c6d836ae1065a8b76938e4cb34776cc1bece
SAHIL0455 <sahilpawar1004@gmail.com>
2022-06-09 14:16:34 +0530

    ADDED: adding a new contact in address book

--- src/com/company/AddressBook.java
+++ src/com/company/AddressBook.java
@@ -7,5 +7,28 @@ public class AddressBook {
 
     public static void main(String[] args) {
         System.out.println("Welcome the Address Book");
+        Contact contact = addContact();
+        System.out.println(contact);
+    }
+
+    private static Contact addContact() {
+        Contact contact = new Contact();
+        System.out.println("Enter the First Name");
+        contact.setFirstName(scanner.next());
+        System.out.println("Enter the Last Name");
+        contact.setLastName(scanner.next());
+        System.out.println("Enter the Address");
+        contact.setAddress(scanner.next());
+        System.out.println("Enter the City");
+        contact.setCity(scanner.next());
+        System.out.println("Enter the State");
+        contact.setState(scanner.next());
+        System.out.println("Enter the Zip");
+        contact.setZip(scanner.nextInt());
+        System.out.println("Enter the Phone Number");
+        contact.setPhoneNumber(scanner.nextLong());
+        System.out.println("Enter the Email ID");
+        contact.setEmail(scanner.next());
+        return contact;
     }
 }
