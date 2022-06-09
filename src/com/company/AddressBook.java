547d1581f35308ec49328627f811c93ea66b085b
SAHIL0455 <sahilpawar1004@gmail.com>
2022-06-09 14:24:13 +0530

    ADDED: ability to edit contact person

--- src/com/company/AddressBook.java
+++ src/com/company/AddressBook.java
@@ -9,6 +9,8 @@ public class AddressBook {
         System.out.println("Welcome the Address Book");
         Contact contact = addContact();
         System.out.println(contact);
+        Contact editContact = editContact(contact);
+        System.out.println(editContact);
     }
 
     private static Contact addContact() {
@@ -29,6 +31,55 @@ public class AddressBook {
         contact.setPhoneNumber(scanner.nextLong());
         System.out.println("Enter the Email ID");
         contact.setEmail(scanner.next());
+        return contact;
+    }
+
+    private static Contact editContact(Contact contact) {
+        System.out.println("Edit the contact \n Select the Option \n1. First Name \n2. Last Name \n3. Address \n4. City \n5. State \n6. Zip \n7. Phone NUmber \n8. Email ID");
+        int option = scanner.nextInt();
+        switch (option) {
+            case 1:
+                System.out.println("Enter the First Name");
+                contact.setFirstName(scanner.next());
+                System.out.println("Now your First Name is : " + contact.getFirstName());
+                break;
+            case 2:
+                System.out.println("Enter the Last Name");
+                contact.setLastName(scanner.next());
+                System.out.println("Now your Last Name IS : " + contact.getLastName());
+                break;
+            case 3:
+                System.out.println("Enter the Address");
+                contact.setAddress(scanner.next());
+                System.out.println("Now Your Address is  : " + contact.getAddress());
+                break;
+            case 4:
+                System.out.println("Enter the City");
+                contact.setCity(scanner.next());
+                System.out.println("Now Your City is : " + contact.getCity());
+                break;
+            case 5:
+                System.out.println("Enter the State");
+                contact.setState(scanner.next());
+                System.out.println("Now Your State is : " + contact.getState());
+                break;
+            case 6:
+                System.out.println("Enter the Zip ");
+                contact.setZip(scanner.nextInt());
+                System.out.println("Now Your Zip is : " + contact.getZip());
+                break;
+            case 7:
+                System.out.println("Enter the Phone Number");
+                contact.setPhoneNumber(scanner.nextLong());
+                System.out.println("Now Your Phone NUmber : " + contact.getPhoneNumber());
+                break;
+            case 8:
+                System.out.println("Enter the Email ID");
+                contact.setEmail(scanner.next());
+                System.out.println("Now Your Email Id : " + contact.getEmail());
+            default:
+                System.out.println("Enter the Valid Number");
+        }
         return contact;
     }
 }
